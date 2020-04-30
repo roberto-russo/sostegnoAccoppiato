f6be7e13420fbcc75a0c83e42e60648964991e63
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-04-30 16:54:47 +0200

    aggiunta classe

---
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/calcoli/entity/ResultCtlUbaMinime.java
@@ -0,0 +1,27 @@
+package it.csi.demetra.demetraws.zoo.calcoli.entity;
+
+public class ResultCtlUbaMinime {
+
+	private boolean result;
+	
+	private boolean errors;
+
+	public boolean isResult() {
+		return result;
+	}
+
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+
+	public boolean isErrors() {
+		return errors;
+	}
+
+	public void setErrors(boolean errors) {
+		this.errors = errors;
+	}
+	
+	
+	
+}
