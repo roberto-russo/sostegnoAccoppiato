a8e131a1a6a85e4e5be679734c6b05a2fe93cd59
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-05-06 17:28:17 +0200

    modifica

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/controlli/visitor/entityRef/ClcInt315Mis5.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/controlli/visitor/entityRef/ClcInt315Mis5.java
@@ -1,6 +1,6 @@
 package it.csi.demetra.demetraws.zoo.controlli.visitor.entityRef;
 
-import java.text.ParseException;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
@@ -11,11 +11,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import it.csi.demetra.demetraws.srmanags.wsbridge2.WSBridgeInternalException_Exception;
 import it.csi.demetra.demetraws.zoo.calcoli.CalcoloException;
 import it.csi.demetra.demetraws.zoo.calcoli.CtlUbaMinime;
 import it.csi.demetra.demetraws.zoo.calcoli.entity.ResultCtlUbaMinime;
 import it.csi.demetra.demetraws.zoo.controlli.visitor.ControlloException;
+import it.csi.demetra.demetraws.zoo.model.Dmt_t_anagrafica_allevamenti;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_clsCapoMacellato;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_contr_loco;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_errore;
@@ -120,7 +120,7 @@ public class ClcInt315Mis5 extends Controllo {
 					 * soggetti, il capo non può essere pagato, salvo rinuncia
 					 * da parte di uno dei richiedenti
 					 */
-					if (duplicatiMacellati == null && (m.getDtInizioDetenzione() != null && m.getDtFineDetenzione() == null) && duplicatiMacellati.isEmpty()) {
+					if (flagDuplicatiRichiedenti(duplicatiMacellati, getAzienda().getCuaa()))  {
 
 						/**
 						 * calcolo sul numero di mesi continuativi in
@@ -166,7 +166,7 @@ public class ClcInt315Mis5 extends Controllo {
 			 * ammissibile a premio
 			 */
 			for (Dmt_t_contr_loco c : estrazioneACampione) {
-				if (!c.getAnomalie_cgo().contains("B")) {
+				if ((c.getAnomalie_cgo() == null) || (c.getAnomalie_cgo().indexOf('B') == -1)) {
 					importoLiquidabile++;
 
 				}
@@ -221,4 +221,36 @@ public class ClcInt315Mis5 extends Controllo {
 		}
 
 	}
+
+	private Boolean flagDuplicatiRichiedenti(List<Dmt_t_clsCapoMacellato> duplicatiMacellati, String cuaa) {
+
+		Dmt_t_anagrafica_allevamenti allev1;
+
+		if (duplicatiMacellati.size() == 1 && duplicatiMacellati.get(0).getCuaa().equals(cuaa))
+			return true;
+		
+		else if (duplicatiMacellati.size() == 2) {
+
+			// se la vacca compare due volte nello stesso allevamento, controllare chi è il
+			// proprietario e chi è il detentore
+			if (duplicatiMacellati.get(0).getAllevId().equals(duplicatiMacellati.get(1).getAllevId())) {
+
+				allev1 = getControlliService()
+						.getAnagraficaByIdAllevamento(BigDecimal.valueOf(duplicatiMacellati.get(0).getAllevId()));
+
+				if (((!allev1.getCod_fiscale_deten().equals(null))
+						&& (allev1.getCod_fiscale_deten().equals(duplicatiMacellati.get(0).getCuaa())
+								&& allev1.getCodFiscaleProp().equals(duplicatiMacellati.get(1).getCuaa())))
+						|| ((!allev1.getCod_fiscale_deten().equals(null))
+								&& (allev1.getCod_fiscale_deten().equals(duplicatiMacellati.get(1).getCuaa())
+										&& allev1.getCodFiscaleProp().equals(duplicatiMacellati.get(0).getCuaa()))))
+					if(allev1.getCod_fiscale_deten().equals(cuaa))
+						return true;
+				
+			} 
+		} 
+			
+		return false;
+	}
+
 }
