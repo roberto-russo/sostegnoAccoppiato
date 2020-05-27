3c5e33236c0243bf2d88b3410af9305ff8529170
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-05-27 16:29:35 +0200

    sviluppo ref03

---
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/beans/Dmt_t_importo_unitario_bean.java
@@ -0,0 +1,13 @@
+package it.csi.demetra.demetraws.zoo.beans;
+
+import java.io.Serializable;
+
+public class Dmt_t_importo_unitario_bean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7854600426303248346L;
+
+	public Dmt_t_importo_unitario_bean() {}
+}
