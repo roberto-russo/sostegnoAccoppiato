9d9e97668118e724e04f5121670e85dc0483ef1d
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-05-27 16:30:50 +0200

    sviluppo ref03

---
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_output_ref03_repository.java
@@ -0,0 +1,13 @@
+package it.csi.demetra.demetraws.zoo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import it.csi.demetra.demetraws.zoo.compositeIds.Dmt_t_output_ref03_id;
+import it.csi.demetra.demetraws.zoo.model.Dmt_t_output_ref03;
+
+@Repository
+public interface Dmt_t_output_ref03_repository extends CrudRepository<Dmt_t_output_ref03, Dmt_t_output_ref03_id>{
+
+	
+}
