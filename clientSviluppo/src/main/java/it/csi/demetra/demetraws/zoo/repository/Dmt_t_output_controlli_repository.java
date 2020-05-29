46d7a6de18398c1e95c4607a82737db280e2d793
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-05-29 16:17:42 +0200

    modifica ref03

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_output_controlli_repository.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_output_controlli_repository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 
 import it.csi.demetra.demetraws.zoo.compositeIds.Dmt_t_output_controlli_id;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_output_controlli;
+import it.csi.demetra.demetraws.zoo.model.Dmt_t_output_ref03;
+import it.csi.demetra.demetraws.zoo.model.Dmt_t_sessione;
 
 @Repository
 public interface Dmt_t_output_controlli_repository extends CrudRepository<Dmt_t_output_controlli, Dmt_t_output_controlli_id> {
@@ -17,5 +19,12 @@ public interface Dmt_t_output_controlli_repository extends CrudRepository<Dmt_t_
 	@Query( value = "select * from dmt_t_output_controlli select capo_id from dmt_t_output_esclusi where sessione = :sessione and calcolo LIKE CONCAT('%',:calcolo,'%') ",
 			nativeQuery = true
 			)
-	List<Dmt_t_output_controlli> findBySessioneAndCalcolo(@Param("sessione") Long sessione, @Param("calcolo") String calcolo); 
+	List<Dmt_t_output_controlli> findBySessioneAndCalcolo(@Param("sessione") Long sessione, @Param("calcolo") String calcolo);
+	
+
+	@Query(
+			value = "SELECT * FROM dmt_t_output_controlli WHERE SESSIONE = :sessione AND cuaa = :cuaa AND ANNO_CAMPAGNA = :anno AND intervento = :codiceIntervento ",
+			nativeQuery = true
+			)
+	Dmt_t_output_controlli findBySessioneAndCuaaAndAnnoCampagnaAndIntervento(@Param("sessione") Dmt_t_sessione sessione, @Param("cuaa") String cuaa, @Param("anno") Long anno, @Param("codiceIntervento") String codiceIntervento);
 }
