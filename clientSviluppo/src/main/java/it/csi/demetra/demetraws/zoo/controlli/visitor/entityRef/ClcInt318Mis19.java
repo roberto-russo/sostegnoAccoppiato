fb41acfd75d19f95de393de55aba7176ca89d19b
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-05-30 12:09:54 +0200

    aggiunta/modifica commenti per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/controlli/visitor/entityRef/ClcInt318Mis19.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/controlli/visitor/entityRef/ClcInt318Mis19.java
@@ -27,7 +27,9 @@ import it.csi.demetra.demetraws.zoo.model.Dmt_t_output_esclusi;
 
 @Component("ClcInt318Mis19")
 /**
- * Classe per il calcolo intervento 318 misura 19
+ * Il caso d’uso indica i controlli da applicare per il calcolo del premio zootecnia per l’intervento 318 – Misura 19:
+ * capi bovini macellati di età compresa tra i 12 e 24 mesi aderenti a sistemi di etichettatura facoltativa riconosciuti
+ * e allevati dal richiedente per un periodo non inferiore a sei mesi prima della macellazione.
  * @author Bcsoft
  *
  */
@@ -52,7 +54,10 @@ public class ClcInt318Mis19 extends Controllo {
 
 	@Override
 	/**
-	 * metodo in cui vengono recuperati i dati, provenienti dalla BDN, dal db e vengono elaborati i controlli massivamente per soggetto
+	 * nel metodo preEsecuzione vengono effettuate due operazioni principali. La prima è l'inizializzazione delle variabili di classe.
+	 * La seconda è l'esecuzione dei controlli di preammissibilità trasversali. Il risultato di tali controlli pregiudica l'esecuzione
+	 * del calcolo stesso. Se l'esecuzione ha esito positivo, allora si può procedere con il calcolo intervento 318 misura 19.
+	 * Se l'esecuzione ha esito negativo, allora viene generato un messaggio di errore.
 	 */
 	public void preEsecuzione() throws ControlloException {
 		this.listaCapiMacellati = null;
@@ -87,7 +92,7 @@ public class ClcInt318Mis19 extends Controllo {
 
 	@Override
 	/**
-	 * metodo in cui vengono eseguiti i controlli per il calcolo intervento 318 misura 19.
+	 * nel metodo esecuzione vengono eseguiti i controlli per il calcolo intervento 318 misura 19.
 	 * Se i controlli per il suddetto calcolo risultano essere positivi, allora viene incrementato il contatore di capi ammissibili
 	 * e il capo sarà visibile in @see Dmt_t_output_controlli. Qualora i capi risultassero non idonei al premio in questione,
 	 * verrà incrementato il numero di capi non ammessi a premio e tale capo sarà inserito nella lista di capi non ammessi a premio. 
@@ -181,7 +186,7 @@ public class ClcInt318Mis19 extends Controllo {
 
 	@Override
 	/**
-	 * metodo in cui vengono salvati a db i dati relativi ai capi ammessi a premio in @see Dmt_t_output_controlli
+	 * nel metodo postEsecuzione vengono salvati a db i dati relativi ai capi ammessi a premio in @see Dmt_t_output_controlli
 	 * e i dati relativi ai capi non ammessi a premio in @see Dmt_t_output_esclusi.
 	 * Dei capi non ammessi a premio sarà salvata l'informazione di identificazione del capo, il premio per cui 
 	 * è stata effettuata la richiesta di amissione e la motivazione per cui  risulta non idoneo al premio.
@@ -226,7 +231,7 @@ public class ClcInt318Mis19 extends Controllo {
 	}
 
 	/**
-	 * metodo in cui viene calcolata la differenza in mesi tra due date
+	 * nel metodo differenzaMesi viene calcolata la differenza in mesi tra due date
 	 * @param dataInizio per dataInizio si intende la prima data da inserire per poter effettuare il calcolo.
 	 * @param dataFine per dataFine si intende la seconda data da inserire per poter effettuare il calcolo.
 	 * il metodo calcolerà i mesi che intercorrono tra la prima e la seconda data.
@@ -238,6 +243,16 @@ public class ClcInt318Mis19 extends Controllo {
 		long monthsBetween = ChronoUnit.MONTHS.between(data1, data2);
 		return monthsBetween;
 	}
+	
+	/**
+	 * nel metodo flagDuplicatiRichiedenti viene analizzata la lista dei cuaa che effettuano una richiesta sullo stesso capo.
+	 * Qualora lo stesso capo sia richiesto in pagamento da due soggetti, il capo non può essere pagato, salvo rinuncia da parte di uno dei richiedenti. 
+	 * Il premio alla macellazione viene riconosciuto ai proprietari/detentori dei capi macellati ed in caso di richiesta di aiuti da parte di entrambi, 
+	 * i capi ammissibili sono pagati esclusivamente al detentore.
+	 * @param duplicatiMacellati lista delle richieste effettuate sullo stesso capo
+	 * @param cuaa codice fiscale del richiedente analizzato
+	 * @return boolean true se il capo può essere pagato al cuaa analizzato, false altrimenti
+	 */
 	private Boolean flagDuplicatiRichiedenti(List<Dmt_t_clsCapoMacellato> duplicatiMacellati, String cuaa) {
 
 		Dmt_t_anagrafica_allevamenti allev1;
