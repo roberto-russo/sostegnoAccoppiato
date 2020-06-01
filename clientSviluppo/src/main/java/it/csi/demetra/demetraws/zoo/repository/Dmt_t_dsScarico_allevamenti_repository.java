c74a0af4bf4dae9e8e8fdbb148611a65fd0fab4f
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-01 15:35:33 +0200

    aggiunta commenti per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_dsScarico_allevamenti_repository.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_dsScarico_allevamenti_repository.java
@@ -11,11 +11,23 @@ import org.springframework.stereotype.Repository;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_dsScarico_allevamenti;
 
 @Repository
+/**
+ * interfaccia che estende CrudRepository in cui sono definiti i metodi di persistenza degli oggetti
+ * di tipo @see Dmt_t_dsScarico_allevamenti e le query ad esso associate.
+ * @author Bcsoft
+ */
 public interface Dmt_t_dsScarico_allevamenti_repository extends CrudRepository<Dmt_t_dsScarico_allevamenti, BigDecimal>{
 	
+	/**
+	 * query che ritorna la lista delle istanze che persistono a DB di tipo Dmt_t_dsScarico_allevamenti
+	 */
 	List<Dmt_t_dsScarico_allevamenti>findAll();
 	
 	@Query(value = "SELECT * FROM dmt_t_dsScarico_allevamenti WHERE allev_id = :allevId", nativeQuery = true)
+	/**
+	 * query che ritorna un'istanza di tipo Dmt_t_dsScarico_allevamenti in base all'allevId
+	 * @param allevId identificativo dell'allevamento
+	 * @return Dmt_t_dsScarico_allevamenti
+	 */
 	Dmt_t_dsScarico_allevamenti findByAllevId(@Param("allevId")long allevId);
-
 }
