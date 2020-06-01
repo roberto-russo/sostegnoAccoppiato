122e941ea4138106b549c722b6a24cfc4cb6833f
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-01 15:35:46 +0200

    aggiunta commenti per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_errore_repository.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_errore_repository.java
@@ -9,7 +9,15 @@ import it.csi.demetra.demetraws.zoo.compositeIds.Dmt_t_errore_id;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_errore;
 
 @Repository
+/**
+ * interfaccia che estende CrudRepository in cui sono definiti i metodi di persistenza degli oggetti
+ * di tipo @see Dmt_t_errore e le query ad esso associate.
+ * @author Bcsoft
+ */
 public interface Dmt_t_errore_repository extends JpaRepository<Dmt_t_errore, Dmt_t_errore_id> {
 
+	/**
+	 * query che ritorna la lista delle istanze che persistono a DB di tipo Dmt_t_errore
+	 */
     List<Dmt_t_errore> findAll();
 }
