d0e7d90c5f93914b737de35191639f82e37e94a8
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-01 15:34:38 +0200

    aggiunta messaggi per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_anagrafica_allevamenti_repository.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_anagrafica_allevamenti_repository.java
@@ -11,17 +11,41 @@ import org.springframework.stereotype.Repository;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_anagrafica_allevamenti;
 
 @Repository
+/**
+ * interfaccia che estende CrudRepository in cui sono definiti i metodi di persistenza degli oggetti
+ * di tipo @see Dmt_t_anagrafica_allevamenti e le query ad esso associate.
+ * @author Bcsoft
+ */
 public interface Dmt_t_anagrafica_allevamenti_repository extends CrudRepository<Dmt_t_anagrafica_allevamenti, Long>{
 	
+	/**
+	 * query che ritorna la lista delle istanze che persistono a DB di tipo Dmt_t_anagrafica_allevamenti
+	 */
 	List<Dmt_t_anagrafica_allevamenti>findAll();
 	
 	@Query(value = "SELECT * FROM Dmt_t_anagrafica_allev WHERE allev_id = :allevId", nativeQuery = true)
+	/**
+	 * query che ritorna l'istanza di tipo Dmt_t_anagrafica_allevamenti in base all'identificativo dell'allevamento
+	 * @param allevId identificativo dell'allevamento
+	 * @return Dmt_t_anagrafica_allevamenti
+	 */
 	Dmt_t_anagrafica_allevamenti findByAllevId(@Param("allevId")BigDecimal allevId);
 	
 	@Query(value = "SELECT * FROM Dmt_t_anagrafica_allev WHERE cod_fiscale_prop = :cuaa ", nativeQuery = true)
+	/**
+	 * query che ritorna la lista delle listanze di tipo Dmt_t_anagrafica_allevamenti in base al codice fiscale del proprietario dell'allevamento
+	 * @param cuaa codice fiscale del proprietario dell'allevamento
+	 * @return List<Dmt_t_anagrafica_allevamenti>
+	 */
 	List <Dmt_t_anagrafica_allevamenti> findPropByCuaa(@Param("cuaa")String cuaa);
 
 	@Query(value = "SELECT cod_fiscale_deten FROM Dmt_t_anagrafica_allev WHERE allev_id = :allevId ", nativeQuery = true)
+	/**
+	 * query che ritorna il codice fiscale del detentore degli aimali presenti in allevamento in base all'identificativo dell'allevamento
+	 * per cui risulta detentore
+	 * @param allevId identificativo dell'allevamento
+	 * @return cod_fiscale_deten
+	 */
 	String findDeteByCuaa(@Param("allevId")BigDecimal allevId);
 	
 }
