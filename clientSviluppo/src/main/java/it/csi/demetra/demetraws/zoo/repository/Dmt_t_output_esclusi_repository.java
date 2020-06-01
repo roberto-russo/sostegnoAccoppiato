55a6e55d5b16191300c77fd1b46fba8e0b427b98
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-01 15:36:28 +0200

    aggiunta commenti per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_output_esclusi_repository.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_output_esclusi_repository.java
@@ -11,6 +11,11 @@ import it.csi.demetra.demetraws.zoo.compositeIds.Dmt_t_output_esclusi_id;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_output_esclusi;
 
 @Repository
+/**
+ * interfaccia che estende CrudRepository in cui sono definiti i metodi di persistenza degli oggetti
+ * di tipo @see Dmt_t_output_esclusi e le query ad esso associate.
+ * @author Bcsoft
+ */
 public interface Dmt_t_output_esclusi_repository extends CrudRepository<Dmt_t_output_esclusi, Dmt_t_output_esclusi_id>{
 
 	
@@ -18,11 +23,23 @@ public interface Dmt_t_output_esclusi_repository extends CrudRepository<Dmt_t_ou
 	@Query( value = "select * from dmt_t_output_esclusi where sessione = :sessione and calcolo LIKE '%' || :calcolo || '%' ",
 			nativeQuery = true
 			)
+	/**
+	 * query che ritorna una lista di istanze di tipo Dmt_t_output_esclusi in base alla sessione e al calcolo
+	 * @param sessione codice di sessione associato all'esecuzione
+	 * @param calcolo codice premio per cui il cuaa effettua la richiesta
+	 * @return List<Dmt_t_output_esclusi>
+	 */
 	List<Dmt_t_output_esclusi> findBySessioneAndCalcolo(@Param("sessione") Long sessione, @Param("calcolo") String calcolo); 
 	
 	@Query(
 			value = "select distinct capo_id from dmt_t_output_esclusi where sessione = :idSessione and calcolo LIKE '%' || :calcolo || '%' ",
 			nativeQuery = true
 		)
+	/**
+	 * query che ritorna una lista di identificativi di capo animale in base alla sessione e al calcolo
+	 * @param idSessione codice di sessione associato all'esecuzione
+	 * @param calcolo codice premio per cui il cuaa effettua la richiesta
+	 * @return List<Long>
+	 */
 	List<Long> isAnomalo(@Param("idSessione") Long idSessione, @Param("calcolo") String calcolo);
 }
