c28145e42631ccf9e16fba66afcd99f4e4f9b3a7
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-01 11:51:50 +0200

    aggiunta commenti per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/model/Dmt_t_certificato_igp_dop.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/model/Dmt_t_certificato_igp_dop.java
@@ -10,14 +10,14 @@ import java.io.Serializable;
 @Table(name = "Dmt_t_certif_igp_dop")
 public class Dmt_t_certificato_igp_dop implements Serializable {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 5666097958872142621L;
 
 
     @Id
     @Column(name = "cuaa", length = 16)
+    /**
+     * Codice fiscale del richiedente
+     */
     private String cuaa;
 
     @Column(name = "flag_igp", length = 1)
@@ -55,26 +55,50 @@ public class Dmt_t_certificato_igp_dop implements Serializable {
         this.zona = zona;
     }
 
+    /**
+     * metodo getter che ritorna il codice fiscale del richiedente
+     * @return cuaa codice fiscale del richiedente.
+     */
     public String getCuaa() {
         return cuaa;
     }
 
+    /**
+     * metodo setter che imposta il codice fiscale del richiedente
+     * @param cuaa codice fiscale del richiedente.
+     */
     public void setCuaa(String cuaa) {
         this.cuaa = cuaa;
     }
 
+    /**
+     * metodo getter che ritorna il flag per l'indicazione geografica protetta
+     * @return flagIgp flag per l'indicazione geografica protetta.
+     */
     public String getFlagIgp() {
         return flagIgp;
     }
 
+    /**
+     * metodo setter che imposta il flag per l'indicazione geografica protetta
+     * @param flagIgp flag per l'indicazione geografica protetta.
+     */
     public void setFlagIgp(String flagIgp) {
         this.flagIgp = flagIgp;
     }
 
+    /**
+     * metodo getter che ritorna il flag di certificato a denominazione di origine protetta
+     * @return flagDop flag di certificato a denominazione di origine protetta.
+     */
     public String getFlagDop() {
         return flagDop;
     }
 
+    /**
+     * metodo setter che imposta il flag di certificato a denominazione di origine protetta
+     * @param flagDop flag di certificato a denominazione di origine protetta.
+     */
     public void setFlagDop(String flagDop) {
         this.flagDop = flagDop;
     }
