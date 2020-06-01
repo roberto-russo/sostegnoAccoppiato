4ec56059439a205ce77902200fce1a877a56804e
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-01 15:34:48 +0200

    aggiunta messaggi per javadoc

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_certificato_igp_dop_repository.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/repository/Dmt_t_certificato_igp_dop_repository.java
@@ -10,10 +10,23 @@ import org.springframework.stereotype.Repository;
 import it.csi.demetra.demetraws.zoo.model.Dmt_t_certificato_igp_dop;
 
 @Repository
+/**
+ * interfaccia che estende CrudRepository in cui sono definiti i metodi di persistenza degli oggetti
+ * di tipo @see Dmt_t_certificato_igp_dop e le query ad esso associate.
+ * @author Bcsoft
+ */
 public interface Dmt_t_certificato_igp_dop_repository extends CrudRepository<Dmt_t_certificato_igp_dop, String>{
 
+	/**
+	 * query che ritorna la lista delle istanze che persistono a DB di tipo Dmt_t_certificato_igp_dop
+	 */
 	List<Dmt_t_certificato_igp_dop>findAll();
 	
 	@Query(value = "select * from Dmt_t_certif_igp_dop where cuaa = :cuaa", nativeQuery = true)
+	/**
+	 * query che ritorna l'istanza di tipo Dmt_t_certificato_igp_dop in base al codice fiscale del richiedente
+	 * @param cuaa codice fiscale del richiedente
+	 * @return Dmt_t_certificato_igp_dop
+	 */
 	Dmt_t_certificato_igp_dop findByCuaa(@Param("cuaa") String cuaa);
 }
