e03153f858f053f157ef378fb172cff7493e2a2a
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 19:02:17 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/model incerti/DMT_W_CONTR_PREAM.sql
@@ -0,0 +1,41 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DMT_W_CONTR_PREAM
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_W_CONTR_PREAM" 
+   (	"ID_INTERVENTO" NUMBER(19,0), 
+	"NOME_CLASSE_CALCOLO" VARCHAR2(30 CHAR), 
+	"ORDINE_ESECUZIONE" VARCHAR2(10 CHAR), 
+	"ID_SESSIONE" NUMBER(19,0)
+   ) SEGMENT CREATION DEFERRED 
+
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_W_CONTR_PREAM
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_W_CONTR_PREAM" ON "DEMETRA"."DMT_W_CONTR_PREAM" ("ID_INTERVENTO", "NOME_CLASSE_CALCOLO", "ORDINE_ESECUZIONE", "ID_SESSIONE") 
+
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_W_CONTR_PREAM
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_W_CONTR_PREAM" MODIFY ("ID_INTERVENTO" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_W_CONTR_PREAM" MODIFY ("NOME_CLASSE_CALCOLO" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_W_CONTR_PREAM" MODIFY ("ORDINE_ESECUZIONE" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_W_CONTR_PREAM" MODIFY ("ID_SESSIONE" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_W_CONTR_PREAM" ADD CONSTRAINT "PK_DMT_W_CONTR_PREAM" PRIMARY KEY ("ID_INTERVENTO", "NOME_CLASSE_CALCOLO", "ORDINE_ESECUZIONE", "ID_SESSIONE");
+--------------------------------------------------------
+--  Ref Constraints for Table DMT_W_CONTR_PREAM
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_W_CONTR_PREAM" ADD CONSTRAINT "FK_DMT_T_SESSIONE_32" FOREIGN KEY ("ID_SESSIONE")
+	  REFERENCES "DEMETRA"."DMT_T_SESSIONE" ("ID_SESSIONE") ENABLE;
