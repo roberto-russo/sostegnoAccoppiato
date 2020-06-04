cdbbbc26aed419393978b64ee66a38d72ac79ee1
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 18:55:05 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/DMT_T_SISTEMI_ETICHETTATURA.sql
@@ -0,0 +1,25 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DMT_T_SISTEMI_ETICHETTATURA
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_T_SISTEMI_ETICHETTATURA" 
+   (	"CUAA" VARCHAR2(16 CHAR), 
+	"FLAG_ETICHETTATURA" VARCHAR2(1 CHAR)
+   ) SEGMENT CREATION DEFERRED 
+
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_T_SISTEMI_ETICHETTATURA
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_T_SISTEMI_ETICHETTATURA" ON "DEMETRA"."DMT_T_SISTEMI_ETICHETTATURA" ("CUAA") 
+
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_T_SISTEMI_ETICHETTATURA
+--------------------------------------------------------
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_SISTEMI_ETICHETTATURA" ADD CONSTRAINT "PK_DMT_T_SISTEMI_ETICHETTATURA" PRIMARY KEY ("CUAA");
