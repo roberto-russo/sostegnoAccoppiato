6ffadd6de5d3d8a57b91f4b68b151b29a2e79c03
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 19:01:23 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/model incerti/DMT_T_RAZZE_AMMISSIBILI.sql
@@ -0,0 +1,28 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DMT_T_RAZZE_AMMISSIBILI
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_T_RAZZE_AMMISSIBILI" 
+   (	"SEZIONE_RAZZA" VARCHAR2(20 CHAR), 
+	"ASSOCIAZIONE" VARCHAR2(20 CHAR), 
+	"DENOM_UFF_LG" VARCHAR2(50 CHAR)
+   ) SEGMENT CREATION DEFERRED 
+
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_T_RAZZE_AMMISSIBILI
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_T_RAZZE_AMMISSIBILI" ON "DEMETRA"."DMT_T_RAZZE_AMMISSIBILI" ("SEZIONE_RAZZA") 
+
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_T_RAZZE_AMMISSIBILI
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_T_RAZZE_AMMISSIBILI" MODIFY ("SEZIONE_RAZZA" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_RAZZE_AMMISSIBILI" ADD CONSTRAINT "PK_DMT_T_RAZZE_AMMISSIBILI" PRIMARY KEY ("SEZIONE_RAZZA");
