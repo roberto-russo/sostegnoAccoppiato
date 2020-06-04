590b4bd9cd91f3f9d04500cf9f1f6a22f9d0ad14
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 18:52:55 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/DMT_T_CONTR_LOCO.sql
@@ -0,0 +1,50 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  Drop Table DMT_T_CONTR_LOCO
+--------------------------------------------------------
+
+DROP TABLE "DEMETRA"."DMT_T_CONTR_LOCO";
+
+--------------------------------------------------------
+--  DDL for Table DMT_T_CONTR_LOCO
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_T_CONTR_LOCO" 
+   (	"CUAA" VARCHAR2(16 CHAR), 
+	"MARCHIO" VARCHAR2(14 CHAR), 
+	"ANNO" NUMBER(10,0), 
+	"ANOMALIE_ZOO" NUMBER(10,0), 
+	"ANOMALIE_CGO" VARCHAR2(10 CHAR), 
+	"COD_ASL" VARCHAR2(16 CHAR), 
+	"COD_CAI" VARCHAR2(16 CHAR), 
+	"COD_UTE" VARCHAR2(5 CHAR), 
+	"CUAA_NORMALIZZATO" VARCHAR2(20 CHAR), 
+	"DESCRIZIONE_ANOMALIA" VARCHAR2(400 CHAR), 
+	"DATA_NASCITA" DATE, 
+	"NOTE" VARCHAR2(400 CHAR), 
+	"PROVINCIA" VARCHAR2(25 CHAR), 
+	"PROVINCIA_ISTAT" NUMBER(10,0), 
+	"REGIONE" VARCHAR2(25 CHAR), 
+	"REGIONE_ISTAT" NUMBER(19,0), 
+	"SESSO" VARCHAR2(1 CHAR)
+   ) SEGMENT CREATION DEFERRED 
+
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_T_CONTR_LOCO
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_T_CONTR_LOCO" ON "DEMETRA"."DMT_T_CONTR_LOCO" ("CUAA", "MARCHIO") 
+
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_T_CONTR_LOCO
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_T_CONTR_LOCO" MODIFY ("CUAA" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_CONTR_LOCO" MODIFY ("MARCHIO" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_CONTR_LOCO" ADD CONSTRAINT "PK_DMT_T_CONTR_LOCO" PRIMARY KEY ("CUAA", "MARCHIO");
