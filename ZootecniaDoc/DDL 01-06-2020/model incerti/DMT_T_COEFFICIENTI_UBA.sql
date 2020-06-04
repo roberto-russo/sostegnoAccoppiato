5b3a4fc31118f2c355bf9921ddba7a8ed82a7dfd
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 19:00:00 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/model incerti/DMT_T_COEFFICIENTI_UBA.sql
@@ -0,0 +1,39 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DMT_T_COEFFICIENTI_UBA
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_T_COEFFICIENTI_UBA" 
+   (	"ID_CATEGORIA" NUMBER(19,0), 
+	"ID_SPECIE" NUMBER(19,0), 
+	"ID_SESSIONE" NUMBER(19,0), 
+	"COEFFICIENTE" NUMBER(*,2)
+   ) SEGMENT CREATION DEFERRED 
+
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_T_COEFFICIENTI_UBA
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_T_COEFFICIENTI_UBA" ON "DEMETRA"."DMT_T_COEFFICIENTI_UBA" ("ID_CATEGORIA", "ID_SPECIE", "ID_SESSIONE") 
+
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_T_COEFFICIENTI_UBA
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_T_COEFFICIENTI_UBA" MODIFY ("ID_CATEGORIA" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_COEFFICIENTI_UBA" MODIFY ("ID_SPECIE" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_COEFFICIENTI_UBA" MODIFY ("ID_SESSIONE" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_COEFFICIENTI_UBA" ADD CONSTRAINT "PK_DMT_T_COEFFICIENTI_UBA" PRIMARY KEY ("ID_CATEGORIA", "ID_SPECIE", "ID_SESSIONE");
+--------------------------------------------------------
+--  Ref Constraints for Table DMT_T_COEFFICIENTI_UBA
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_T_COEFFICIENTI_UBA" ADD CONSTRAINT "FK_DMT_T_SESSIONE_20" FOREIGN KEY ("ID_SESSIONE")
+	  REFERENCES "DEMETRA"."DMT_T_SESSIONE" ("ID_SESSIONE") ENABLE;
