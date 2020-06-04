fd03d3233cc0ff0ccc6d007742a8b32ad13bfc72
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 18:53:31 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/DMT_T_LATTE_VENDITA_DIRETTA.sql
@@ -0,0 +1,34 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DMT_T_LATTE_VENDITA_DIRETTA
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_T_LATTE_VENDITA_DIRETTA" 
+   (	"ANNO_CAMPAGNA" NUMBER(10,0), 
+	"CUUA" VARCHAR2(16 CHAR), 
+	"MESE" VARCHAR2(10 CHAR), 
+	"MATRICOLA" VARCHAR2(25 CHAR), 
+	"QUANTITA" NUMBER(19,0)
+   ) SEGMENT CREATION DEFERRED 
+
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_T_LATTE_VENDITA_DIRETTA
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_T_LATTE_VENDITA_DIRETTA" ON "DEMETRA"."DMT_T_LATTE_VENDITA_DIRETTA" ("ANNO_CAMPAGNA", "CUUA", "MESE") 
+
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_T_LATTE_VENDITA_DIRETTA
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_T_LATTE_VENDITA_DIRETTA" MODIFY ("ANNO_CAMPAGNA" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_LATTE_VENDITA_DIRETTA" MODIFY ("CUUA" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_LATTE_VENDITA_DIRETTA" MODIFY ("MESE" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_T_LATTE_VENDITA_DIRETTA" ADD CONSTRAINT "PK_DMT_T_LATTE_VENDITA_DIRETTA" PRIMARY KEY ("ANNO_CAMPAGNA", "CUUA", "MESE");
