75486f82513a59f2e7ef8b5c6ae9d84763a54a3c
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-06-04 18:58:01 +0200

    aggiunta ddl

---
+++ ZootecniaDoc/DDL 01-06-2020/model incerti/DMT_DEC_CATEGORIA.sql
@@ -0,0 +1,34 @@
+--------------------------------------------------------
+--  File creato - mercoledì-giugno-03-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table DMT_DEC_CATEGORIA
+--------------------------------------------------------
+
+  CREATE TABLE "DEMETRA"."DMT_DEC_CATEGORIA" 
+   (	"ID_SESSIONE" NUMBER(19,0), 
+	"DESCRIZIONE" VARCHAR2(255 CHAR), 
+	"ID" NUMBER(19,0)
+   ) SEGMENT CREATION DEFERRED 
+  TABLESPACE "DEMETRA_TBL" ;
+--------------------------------------------------------
+--  DDL for Index IE_DMT_DEC_CATEGORIA
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "DEMETRA"."IE_DMT_DEC_CATEGORIA" ON "DEMETRA"."DMT_DEC_CATEGORIA" ("ID_SESSIONE") 
+  TABLESPACE "DEMETRA_IDX" ;
+--------------------------------------------------------
+--  Constraints for Table DMT_DEC_CATEGORIA
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_DEC_CATEGORIA" MODIFY ("ID_SESSIONE" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_DEC_CATEGORIA" MODIFY ("ID" NOT NULL ENABLE);
+ 
+  ALTER TABLE "DEMETRA"."DMT_DEC_CATEGORIA" ADD CONSTRAINT "PK_DMT_DEC_CATEGORIA" PRIMARY KEY ("ID_SESSIONE");
+--------------------------------------------------------
+--  Ref Constraints for Table DMT_DEC_CATEGORIA
+--------------------------------------------------------
+
+  ALTER TABLE "DEMETRA"."DMT_DEC_CATEGORIA" ADD CONSTRAINT "FK_DMT_T_SESSIONE_18" FOREIGN KEY ("ID_SESSIONE")
+	  REFERENCES "DEMETRA"."DMT_T_SESSIONE" ("ID_SESSIONE") ENABLE;
