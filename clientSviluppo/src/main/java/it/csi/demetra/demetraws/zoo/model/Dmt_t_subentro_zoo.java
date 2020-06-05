dcf20ccb8fc5e6693e9b265e3e583ecf49ade62b
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-05 15:23:24 +0200

    modifica model, aggiunta creazione sequence

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/model/Dmt_t_subentro_zoo.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/model/Dmt_t_subentro_zoo.java
@@ -5,10 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
