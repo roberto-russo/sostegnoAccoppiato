29035f857880a0eb8c080d656e53b10bdc5919be
Schetkiglobe <antonio.pilato@bcsoft.net>
2020-06-05 15:23:14 +0200

    modifica model, aggiunta creazione sequence

--- clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/model/Dmt_t_razze_ammissibili.java
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/model/Dmt_t_razze_ammissibili.java
@@ -2,8 +2,6 @@ package it.csi.demetra.demetraws.zoo.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
