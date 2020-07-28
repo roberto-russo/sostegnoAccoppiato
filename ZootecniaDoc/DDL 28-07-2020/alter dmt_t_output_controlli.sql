efc6670788dd546e6920b3ce89cb26918a7509f7
giuseppepennetta <61731833+giuseppepennetta@users.noreply.github.com>
2020-07-28 17:37:38 +0200

    alter tabelle di output

---
+++ ZootecniaDoc/DDL 28-07-2020/alter dmt_t_output_controlli.sql
@@ -0,0 +1,2 @@
+alter table dmt_t_output_controlli add capi_anomali_pagabili NUMBER(10,0); 
+alter table dmt_t_output_controlli add capi_anomali_non_pagabili NUMBER(10,0);
---
+++ ZootecniaDoc/DDL 28-07-2020/alter dmt_t_output_ref03.sql
@@ -0,0 +1,2 @@
+alter table dmt_t_output_ref03 rename COLUMN capi_anomali to capi_anomali_pagabili; 
+alter table dmt_t_output_ref03 add capi_anomali_non_pagabili NUMBER(10,0); 
