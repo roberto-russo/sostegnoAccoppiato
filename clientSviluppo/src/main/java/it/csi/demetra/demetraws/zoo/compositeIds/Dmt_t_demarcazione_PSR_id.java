8c4411717bc6bd48654d92b13ce847a3a21aba30
Schetkiglobe7 <antonio.pilato96@outlook.com>
2020-09-28 15:57:11 +0200

    modifiche zootecnia per il calcolo dei capi dichiarati

---
+++ clientSviluppo/src/main/java/it/csi/demetra/demetraws/zoo/compositeIds/Dmt_t_demarcazione_PSR_id.java
@@ -0,0 +1,56 @@
+package it.csi.demetra.demetraws.zoo.compositeIds;
+
+import java.io.Serializable;
+
+public class Dmt_t_demarcazione_PSR_id implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2720017660835828783L;
+	
+	private Integer anno;
+	private String marchioAuricolare;
+	
+	public Dmt_t_demarcazione_PSR_id() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dmt_t_demarcazione_PSR_id(Integer anno, String marchioAuricolare) {
+		super();
+		this.anno = anno;
+		this.marchioAuricolare = marchioAuricolare;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((anno == null) ? 0 : anno.hashCode());
+		result = prime * result + ((marchioAuricolare == null) ? 0 : marchioAuricolare.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dmt_t_demarcazione_PSR_id other = (Dmt_t_demarcazione_PSR_id) obj;
+		if (anno == null) {
+			if (other.anno != null)
+				return false;
+		} else if (!anno.equals(other.anno))
+			return false;
+		if (marchioAuricolare == null) {
+			if (other.marchioAuricolare != null)
+				return false;
+		} else if (!marchioAuricolare.equals(other.marchioAuricolare))
+			return false;
+		return true;
+	}
+}
